768e1a19d83fa1f8bc3b0951624638f93a6b7bb3
Odair <odairtopper@gmail.com>
2022-02-03 17:57:03 -0300

    Desenvolvendo
    Estudando mais sobre states e começando a estudar sobre React Hooks. Continuando o curso

--- Meme_Generator/components/MemeForm.jsx
+++ Meme_Generator/components/MemeForm.jsx
@@ -9,44 +9,65 @@ export function Form(props){
     BottomText: ""
   })
 
+  const [Texts, setTexts] = React.useState({
+    above: "",
+    below: "",
+    imageOn:false,
+  })
+
   function GetRandomMeme(){
     setmeme(
        {
         TopText: document.getElementById("above").value,
         bottomText: document.getElementById("below").value,
-        memeUrl: Allmemes[Math.floor(Math.random() * Allmemes.length)].url
+        memeUrl: Allmemes[Math.floor(Math.random() * Allmemes.length)].url  
     })
-    
 
+    setTexts(() =>({...Texts, imageOn: true}))
+  }
+  function handleChange(event){
+    const {name, value} = event.target
+      setTexts(prevTexts =>({...prevTexts, [name]: value}))
   }
 
   return(
     <div >
       <div className="Form">
-        <input 
-          type="text" 
-          name="above" 
-          id="above" 
-          Placeholder="above text" 
-          className="Form--Input"/>
-       
-        <input 
-          type="text" 
-          name="below" 
-          id="below" 
-          Placeholder="below text" 
-          className="Form--Input"/>
-       
+
+          <input 
+            type="text" 
+            name="above" 
+            id="above" 
+            value={Texts.above} 
+            onChange={handleChange}
+            Placeholder="above text" 
+            className="Form--Input"/>
+        
+          <input 
+            type="text" 
+            name="below" 
+            id="below"
+            value={Texts.below} 
+            onChange={handleChange} 
+            Placeholder="below text" 
+            className="Form--Input"/>
+     
         <input 
           type="submit" 
           name="below"
           className="btn" 
+          id="BtnMeme"
           value="Get a meme image    🖼"
           onClick={GetRandomMeme}/>
+
       </div>
 
       <div className="center">
         <img className="Meme-Photo" src={meme.memeUrl} alt="" />
+        <div className="text">
+          <h2 className="meme--text top">{Texts.imageOn?  Texts.above : ""}</h2>
+          <h2 className="meme--text bottom">{Texts.imageOn?  Texts.below : "" }</h2>
+        </div>
       </div>
       
     </div>
